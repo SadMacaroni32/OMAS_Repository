eccd4f1dfb621404e9b704d48ba1247b6a69bc58
Dale <unitejohndalev@gmail.com>
2024-04-02 15:00:12 +0800

    landing and login UI

--- FRONTEND/src/Integration.tsx
+++ FRONTEND/src/Integration.tsx
@@ -1,12 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import LandingPage from "./components/login/Index"
 
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -9,25 +9,28 @@ import store from "./redux/store/store";
 import { Global, css } from "@emotion/react";
 import Integration from "./Integration";
 
-const globalStyles = css`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-  * {
-  font-family: "Roboto", sans-serif;
-//   color: red;
-}
+import { BrowserRouter as Router } from "react-router-dom";
 
+const globalStyles = css`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+  * {
+    font-family: "Roboto", sans-serif;
+    //   color: red;
+  }
 `;
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-	// <React.StrictMode>
-	<Provider store={store}>
-		<Global styles={globalStyles} />
-		<Integration />
-	</Provider>
-	// </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <Global styles={globalStyles} />
+        <Integration />
+      </Provider>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
