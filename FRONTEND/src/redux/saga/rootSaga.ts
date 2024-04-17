eaf550effea454c5c1b7c8fdff756a443eb4e109
SadMacaroni32 <flash.staffsuport@gmail.com>
2024-04-18 07:09:44 +0800

    Dashboard Fix

--- FRONTEND/src/redux/saga/rootSaga.ts
+++ FRONTEND/src/redux/saga/rootSaga.ts
@@ -33,7 +33,6 @@ export default function* rootSaga() {
     watchFetchUsers(),
     fetchReservationsDate(),
     reservationsSagaDate(),
-    reservationWithUserInfoSaga(),
     getUserIdSaga(),
 
     getRecentCommentsSaga(),
