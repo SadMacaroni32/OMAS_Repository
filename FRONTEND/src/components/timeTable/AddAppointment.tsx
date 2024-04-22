e7c2dba36f8cc7b1cd9a00a89b019baab98405f0
MIKE DAYA <mikeedaya@gmail.com>
2024-04-22 14:40:45 +0800

    add appointmentv2

--- FRONTEND/src/components/timeTable/AddAppointment.tsx
+++ FRONTEND/src/components/timeTable/AddAppointment.tsx
@@ -1,20 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { Grid, Typography, TextField, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
-import { addReservation } from '../../redux/state/addReservationActions';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../redux/store/store';
-import { fetchReservationsRequest } from '../../redux/state/reservationState';
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import { addReservation } from "../../redux/state/addReservationActions";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store/store";
+import { fetchReservationsRequest } from "../../redux/state/reservationState";
 
 const AddAppointment = ({ seat_id }) => {
-  const [startDate, setStartDate] = useState('');
-  const [startTime, setStartTime] = useState('06:00');
-  const [endDate, setEndDate] = useState('');
-  const [endTime, setEndTime] = useState('12:30');
-  const [note, setNote] = useState('');
+  const [startDate, setStartDate] = useState("");
+  const [startTime, setStartTime] = useState("06:00");
+  const [endDate, setEndDate] = useState("");
+  const [endTime, setEndTime] = useState("12:30");
+  const [note, setNote] = useState("");
   const [openModal, setOpenModal] = useState(false); // State for modal
   const dispatch = useDispatch();
-  
-  const reservations = useSelector((state: RootState) => state.reservationsReducer.reservations);
+
+  const reservations = useSelector(
+    (state: RootState) => state.reservationsReducer.reservations
+  );
 
   useEffect(() => {
     dispatch(fetchReservationsRequest());
@@ -34,8 +50,8 @@ const AddAppointment = ({ seat_id }) => {
     const selectedStartTime = event.target.value;
     setStartTime(selectedStartTime);
 
-    if (selectedStartTime === '12:30' && startDate === endDate) {
-      setEndTime('19:30');
+    if (selectedStartTime === "12:30" && startDate === endDate) {
+      setEndTime("19:30");
     }
   };
 
@@ -62,59 +78,119 @@ const AddAppointment = ({ seat_id }) => {
       alert("Please fill in all fields.");
       return;
     } else {
-      console.log('Start Date:', startDate);
-      console.log('Start Time:', startTime);
-      console.log('End Date:', endDate);
-      console.log('End Time:', endTime);
-      console.log('Note:', note);
+      console.log("Start Date:", startDate);
+      console.log("Start Time:", startTime);
+      console.log("End Date:", endDate);
+      console.log("End Time:", endTime);
+      console.log("Note:", note);
 
-      dispatch(addReservation(seat_id, startDate, startTime, endDate, endTime, note));
+      dispatch(
+        addReservation(seat_id, startDate, startTime, endDate, endTime, note)
+      );
       setOpenModal(true); // Open the modal after submitting
-      setStartDate('');
-      setEndDate('');
-      setNote('');
-      setStartTime('06:00');
-      setEndTime('12:30');
+      setStartDate("");
+      setEndDate("");
+      setNote("");
+      setStartTime("06:00");
+      setEndTime("12:30");
     }
   };
 
   const handleCloseModal = () => {
     setOpenModal(false); // Close the modal
   };
-  
- 
 
-console.log("Filtered Reservations:", reservations.filter((reservation) => reservation.seat_id === seat_id));
-console.log("SEAT ID:", seat_id);
- console.log("MGA LIST",reservations);
+  const reservedDates = reservations
+    .filter((reservation) => reservation.seat_id === seat_id)
+    .map((reservation) => {
+      const rstartDate = new Date(reservation.start_date);
+      const rendDate = new Date(reservation.end_date);
+
+      return {
    
+        rstartDate: new Date(
+          Date.UTC(
+            rstartDate.getFullYear(),
+            rstartDate.getMonth(),
+            rstartDate.getDate(),
+            0,
+            0,
+            0
+          )
+        ), // Set time to 06:00:00 UTC
+        rendDate: new Date(
+          Date.UTC(
+            rendDate.getFullYear(),
+            rendDate.getMonth(),
+            rendDate.getDate(),
+            0,
+            0,
+            0
+          )
+        ), // Set time to 06:00:00 UTC
+        reservationId: reservation.reservation_id, // Include the reservation_id
+      };
+    });
+
+  console.log("TRY", reservedDates);
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        <Typography variant="h5">Add Appointment for Seat No.{seat_id}</Typography>
+        <Typography variant="h5">
+          Add Appointment for Seat No.{seat_id}
+        </Typography>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <TextField
-          label="Start Date"
-          type="date"
-          value={startDate}
-          onChange={handleStartDateChange}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          fullWidth
-          // Disable picking previous dates and the reserved dates
-          inputProps={{
-            min: new Date().toISOString().split('T')[0], // Set the minimum date to today
-          }}
-        />
+      <TextField
+  label="Start Date"
+  type="date"
+  value={startDate}
+  onChange={handleStartDateChange}
+  InputLabelProps={{
+    shrink: true,
+  }}
+  fullWidth
+  inputProps={{
+    min: new Date().toISOString().split("T")[0], // Set the minimum date to today
+   
+    shouldDisableDate: (date) => {
+      const selectedDate = new Date(date); // Convert the selected date to a Date object
+      console.log('Selected Date:', selectedDate);
+      reservedDates.forEach((reservedDate) => {
+        console.log('Reserved Start Date:', reservedDate.rstartDate);
+        console.log('Reserved End Date:', reservedDate.rendDate);
+      });
+      const shouldDisable = reservedDates.some((reservedDate) => {
+        return (
+          selectedDate >= reservedDate.rstartDate &&
+          selectedDate <= reservedDate.rendDate
+        );
+      });
+      console.log('Should Disable:', shouldDisable);
+      return shouldDisable;
+    },
+  }}
+/>
+
+
+
+
+
       </Grid>
       <Grid item xs={12} sm={6}>
         <FormControl component="fieldset">
           <FormLabel component="legend">Start Time</FormLabel>
           <RadioGroup row value={startTime} onChange={handleStartTimeChange}>
-            <FormControlLabel value="06:00" control={<Radio />} label="6:00 AM" />
-            <FormControlLabel value="12:30" control={<Radio />} label="12:30 PM" />
+            <FormControlLabel
+              value="06:00"
+              control={<Radio />}
+              label="6:00 AM"
+            />
+            <FormControlLabel
+              value="12:30"
+              control={<Radio />}
+              label="12:30 PM"
+            />
           </RadioGroup>
         </FormControl>
       </Grid>
@@ -128,21 +204,26 @@ console.log("SEAT ID:", seat_id);
             shrink: true,
           }}
           fullWidth
-          // Disable picking previous dates and the reserved dates
           inputProps={{
-            min: startDate || new Date().toISOString().split('T')[0], // Set the minimum date to today or selected start date
-          
-          }}
+            min: startDate || new Date().toISOString().split("T")[0], // Set the minimum date to today or selected start date
+           }}
         />
       </Grid>
       <Grid item xs={12} sm={6}>
         <FormControl component="fieldset">
           <FormLabel component="legend">End Time</FormLabel>
           <RadioGroup row value={endTime} onChange={handleEndTimeChange}>
-            <FormControlLabel value="12:30" control={<Radio />} label="12:30 PM" 
-              disabled={startTime === '12:30' && startDate === endDate} // Disable if start time is '12:30'
+            <FormControlLabel
+              value="12:30"
+              control={<Radio />}
+              label="12:30 PM"
+              disabled={startTime === "12:30" && startDate === endDate} // Disable if start time is '12:30'
             />
-            <FormControlLabel value="19:30" control={<Radio />} label="7:30 PM" />
+            <FormControlLabel
+              value="19:30"
+              control={<Radio />}
+              label="7:30 PM"
+            />
           </RadioGroup>
         </FormControl>
       </Grid>
@@ -156,7 +237,9 @@ console.log("SEAT ID:", seat_id);
         />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" color="primary" onClick={handleSubmit}>Submit</Button>
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          Submit
+        </Button>
       </Grid>
       <Dialog open={openModal} onClose={handleCloseModal}>
         <DialogTitle>Appointment Added</DialogTitle>
