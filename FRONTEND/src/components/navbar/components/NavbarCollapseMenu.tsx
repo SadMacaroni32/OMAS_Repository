56d1997b746b36b4521c1a960790d1dd827ec19a
Christine <tinegulmatico@gmail.com>
2024-04-25 08:59:10 +0800

    added the menu items to the collapsible navbar menu

--- FRONTEND/src/components/navbar/components/NavbarCollapseMenu.tsx
+++ FRONTEND/src/components/navbar/components/NavbarCollapseMenu.tsx
@@ -24,59 +24,89 @@ export default function NavbarCollapseMenu() {
   const { anchorElUser, handleOpenUserMenu, handleCloseUserMenu, pageLogout } =
     ScriptsNav();
 
+  const generateAvatarFromName = (name: string) => {
+    const initials = name
+      .split(" ")
+      .map((word) => word[0])
+      .join("");
+    return <Avatar>{initials}</Avatar>;
+  };
+
   // console.log("Navbar Collapse Menu", loggedUser)
 
   return (
     <>
       {/* COLLAPSE MENU */}
       {loggedUser && (
-      <Box sx={{ flexGrow: 0 }}>
-        <Tooltip title="Open settings">
-          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-          </IconButton>
-        </Tooltip>
-        <Menu
-          sx={{ mt: "45px" }}
-          id="menu-appbar"
-          anchorEl={anchorElUser}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={Boolean(anchorElUser)}
-          onClose={handleCloseUserMenu}
-        >
-          <Box
-            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', m: 1 }}
-            onClick={() => {
-              handleCloseUserMenu();
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar>
+                {generateAvatarFromName(
+                  loggedUser.first_name + " " + loggedUser.last_name
+                )}
+              </Avatar>
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
             }}
-          >
-            <Typography id="name" textAlign="center" sx={{fontSize: '1rem', fontWeight: "bold", color: "#25476A"}}>
-              {loggedUser.first_name} {loggedUser.middle_name} {loggedUser.last_name}
-            </Typography>
-            <Typography id="position" textAlign="center" sx={{fontSize: '1rem', color: "#25476A"}}>
-              {loggedUser.position_name}
-            </Typography>
-          </Box>
-          {/* Logout */}
-          <MenuItem
-          sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-            onClick={() => {
-              handleCloseUserMenu();
-              pageLogout();
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
             }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
           >
-            <Typography textAlign="center">Logout</Typography>
-          </MenuItem>
-        </Menu>
-      </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                m: 1,
+              }}
+              onClick={() => {
+                handleCloseUserMenu();
+              }}
+            >
+              <Typography
+                id="name"
+                textAlign="center"
+                sx={{ fontSize: "1rem", fontWeight: "bold", color: "#25476A" }}
+              >
+                {loggedUser.first_name} {loggedUser.middle_name}{" "}
+                {loggedUser.last_name}
+              </Typography>
+              <Typography
+                id="position"
+                textAlign="center"
+                sx={{ fontSize: "1rem", color: "#25476A" }}
+              >
+                {loggedUser.position_name}
+              </Typography>
+            </Box>
+            {/* Logout */}
+            <MenuItem
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+              onClick={() => {
+                handleCloseUserMenu();
+                pageLogout();
+              }}
+            >
+              <Typography textAlign="center">Logout</Typography>
+            </MenuItem>
+          </Menu>
+        </Box>
       )}
     </>
   );
